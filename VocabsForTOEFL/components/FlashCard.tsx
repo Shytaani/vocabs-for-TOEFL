047d337223334cb918d9ce2ead87baffd5e5b86e
Tanishy <mambaout134@gmail.com>
2022-01-07 23:48:00 +0900

    Change card's color

--- VocabsForTOEFL/components/FlashCard.tsx
+++ VocabsForTOEFL/components/FlashCard.tsx
@@ -12,7 +12,7 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     marginHorizontal: 20,
     paddingHorizontal: 10,
-    backgroundColor: 'antiquewhite',
+    backgroundColor: 'lightskyblue',
   },
   cardFront: {
     position: 'absolute',
