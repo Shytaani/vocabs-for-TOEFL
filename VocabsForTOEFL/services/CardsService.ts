64d86c588a0c2fb63fecfe3e741d8fdbcea94ad7
Tanishy <mambaout134@gmail.com>
2022-01-14 01:02:35 +0900

    Enable saving current progress using AsyncStorage

--- VocabsForTOEFL/services/CardsService.ts
+++ VocabsForTOEFL/services/CardsService.ts
@@ -1,5 +1,6 @@
 import { Card } from '../types/Card';
 import data from '../assets/card-contents.json';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export function getWords(): Card[] {
   return data;
@@ -7,4 +8,16 @@ export function getWords(): Card[] {
 
 export function getWord(id: number): Card {
   return data[id];
+}
+
+export async function saveCurrentWordId(id: number): Promise<void> {
+  await AsyncStorage.setItem('current', id.toString());
+}
+
+export async function getCurrentWordId(): Promise<number> {
+  const id = await AsyncStorage.getItem('current');
+  if (id === null) {
+    return 0;
+  }
+  return Number.parseInt(id, 10);
 }
